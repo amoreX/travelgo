8f827b5d68b401e428b86e0d730f0064c1635d10
Md Nihal Rahman <rahmannihal72@gmail.com>
2024-06-29 01:37:36 +0530

    cleaned repo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Landing from "./Components/Landing/Landing.tsx"
+import Content from "./Components/Content/Content.tsx"
+const router=createBrowserRouter([
+  {
+    path: "/",
+    element: <Landing />
+  },
+  {
+    path:"/Content",
+    element: <Content />
+  },
+  
 
+])
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+       <RouterProvider router={router}></RouterProvider>
+    </>
   );
 }
 
---
+++ src/Components/Landing/Landing.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Land: React.FC = () => {
+    return (
+        <div>ok</div>
+    );
+}
+
+export default Land;
