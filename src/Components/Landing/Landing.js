b2cd6e88638f4f38dce78cd6bc1f8e5c5ae98aff
Md Nihal Rahman <rahmannihal72@gmail.com>
2024-08-04 13:14:54 +0530

    fresh-start

---
+++ src/Components/Landing/Landing.js
@@ -0,0 +1,5 @@
+export default function Landing(){
+    return(
+        <div>no</div>
+    )
+}
--- src/Components/Landing/Landing.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Land: React.FC = () => {
-    return (
-        <div>ok</div>
-    );
-}
-
-export default Land;
