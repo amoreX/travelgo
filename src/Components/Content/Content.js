1532ee573f6d48315a829f5a9f699a7dfee14430
Md Nihal Rahman <rahmannihal72@gmail.com>
2024-08-11 18:16:34 +0530

    layout

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -1,5 +1,25 @@
+import "./Content.scss"
+
 export default function Content(){
     return(
-        <div>Content</div>
+        <div id="main-container-content">
+            <div id="container">
+                <div id="noti">
+                    <div id="left">Bengaluru</div>
+                    <div id="right">search</div>
+                </div>
+                <div id="content">
+                    <div id="row-1">
+                        <div id="weather"></div>
+                        <div id="weather-2"></div>
+                        <div id="weather-3"></div>
+                    </div>
+                    <div id="row-2">
+                        <div id="weather"></div>
+                        <div id="weather-2"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
