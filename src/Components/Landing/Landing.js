ae7f07d1454f4611dfeddd610d9a866f6a15ad67
Md Nihal Rahman <rahmannihal72@gmail.com>
2024-08-11 12:39:17 +0530

    auto-suggestion

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,48 +1,41 @@
 //Landing page upon visiting the site
 
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router";
+import {Suggestion} from "./getSuggestions";
 import { motion } from "framer-motion";
+import { search_icon } from "./vars";
 import "./Landing.scss";
 
 export default function Landing() {
-  const search_icon = (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="40px"
-      height="40px"
-      viewBox="0 0 24 24"
-      fill="none"
-    >
-      <defs>
-        <linearGradient id="gradientStroke" x1="0%" y1="0%" x2="100%" y2="100%">
-          <stop offset="0%" style={{ stopColor: "rgba(41, 151, 185, 0.6)" }} />
-          <stop offset="33%" style={{ stopColor: "rgba(58, 101, 184, 0.6)" }} />
-          <stop offset="67%" style={{ stopColor: "rgba(191, 61, 162, 0.6)" }} />
-          <stop
-            offset="100%"
-            style={{ stopColor: "rgba(212, 73, 115, 0.6)" }}
-          />
-        </linearGradient>
-      </defs>
-
-      <path
-        d="M11 6C13.7614 6 16 8.23858 16 11M16.6588 16.6549L21 21M19 11C19 15.4183 15.4183 19 11 19C6.58172 19 3 15.4183 3 11C3 6.58172 6.58172 3 11 3C15.4183 3 19 6.58172 19 11Z"
-        stroke="url(#gradientStroke)"
-        strokeWidth={2}
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
-  );
-  const City_list = ["Mumbai", "Kolkata", "Goa"];
+  const shift = useNavigate();
+  const [cityList,setCityList]=useState(["Mumbai", "Kolkata", "Goa"]);
   const [search, setSearch] = useState("");
-  const navcity = (input) => {
-    console.log(input); // update city to be searched here
+
+  const changePage=(input)=>{
+    //update city name in useContext here
+    console.log(input);
+    setTimeout(()=>{
+      shift("/content");
+    },2000);
   };
 
   useEffect(() => {
-    console.log(search);
+    // console.log(search.length);
+    if (search.length<2){
+      setCityList(["Mumbai","Kolkata","Goa"]);
+    }
+    else{
+      const newSugg=async()=>{
+        let newSuggestions=await  Suggestion(search);
+        console.log(newSuggestions);
+        newSuggestions=newSuggestions.split(",");
+        setCityList(newSuggestions);
+      };
+      newSugg();
+    }
   }, [search]);
+
   return (
     <motion.div
       id="main-container"
@@ -79,14 +72,15 @@ export default function Landing() {
           initial={{ opacity: 0 }}
           transition={{ type: "tween", ease: "easeInOut", delay: 1.5 }}
           animate={{ opacity: 1 }}
+          onClick={()=>changePage(search)}
         >
           {search_icon}
         </motion.div>
       </motion.div>
       <div id="suggestions">
-        {City_list.map((city, index) => {
+        {cityList.map((city, index) => {
           return (
-            <motion.div id="city" key={index} onClick={() => navcity(city)}
+            <motion.div id="city" key={index} onClick={() => {changePage(city);}}
             initial={{y:20,opacity:0}}
             transition={{type:"tween",delay:1.5+index*0.1}}
             animate={{y:0,opacity:1}}
---
+++ src/Components/Landing/vars.js
@@ -0,0 +1,29 @@
+export const search_icon = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="40px"
+      height="40px"
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <defs>
+        <linearGradient id="gradientStroke" x1="0%" y1="0%" x2="100%" y2="100%">
+          <stop offset="0%" style={{ stopColor: "rgba(41, 151, 185, 0.6)" }} />
+          <stop offset="33%" style={{ stopColor: "rgba(58, 101, 184, 0.6)" }} />
+          <stop offset="67%" style={{ stopColor: "rgba(191, 61, 162, 0.6)" }} />
+          <stop
+            offset="100%"
+            style={{ stopColor: "rgba(212, 73, 115, 0.6)" }}
+          />
+        </linearGradient>
+      </defs>
+
+      <path
+        d="M11 6C13.7614 6 16 8.23858 16 11M16.6588 16.6549L21 21M19 11C19 15.4183 15.4183 19 11 19C6.58172 19 3 15.4183 3 11C3 6.58172 6.58172 3 11 3C15.4183 3 19 6.58172 19 11Z"
+        stroke="url(#gradientStroke)"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
