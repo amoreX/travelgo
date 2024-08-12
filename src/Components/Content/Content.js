70a52b16921cbf101c5a1c8e313664d0450af14a
Md Nihal Rahman <rahmannihal72@gmail.com>
2024-08-12 14:10:05 +0530

    animation

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -1,4 +1,5 @@
 import "./Content.scss";
+import { motion } from "framer-motion";
 import Weather from "./weather.js";
 import { useState, useEffect } from "react";
 import { coords } from "../Utils/coords.js";
@@ -7,18 +8,28 @@ export default function Content() {
   let pp = localStorage.getItem("location");
   const [isweather, setIsweather] = useState(false);
   const [weatherData, setWeatherdata] = useState(null);
+  const [transition, setTransition] = useState(false);
   const setWeather = () => {
-    setIsweather(!isweather);
+    if (transition) {
+      setTimeout(() => {
+        setTransition(!transition);
+      }, 400);
+    } else {
+      setTransition(!transition);
+    }
+    setTimeout(() => {
+      setIsweather(!isweather);
+    }, 500);
   };
 
   const gettingdataweather = async () => {
-      const vale = await getcoords(pp);
-      setWeatherdata(await coords(vale?.lat, vale?.lon));
+    const vale = await getcoords(pp);
+    setWeatherdata(await coords(vale?.lat, vale?.lon));
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     gettingdataweather();
-  },[]);
+  }, []);
 
   return (
     <div id="main-container-content">
@@ -31,15 +42,27 @@ export default function Content() {
           <Weather toggle={setWeather} />
         ) : (
           <div id="content">
-            <div id="row-1">
-              <div id="weather" onClick={() => setWeather()}> {weatherData?.current.apparent_temperature} </div>
+            <motion.div
+              id="row-1"
+              initial={{ x: transition == false ? -2000 : 0 }}
+              transition={{ type: "tween", duration: 0.38 }}
+              animate={{ x: transition == false ? 0 : -2000 }}
+            >
+              <div id="weather" onClick={() => setWeather()}>
+                {" "}
+                {weatherData?.current.apparent_temperature}{" "}
+              </div>
               <div id="weather-2"></div>
               <div id="weather-3"></div>
-            </div>
-            <div id="row-2">
+            </motion.div>
+            <motion.div id="row-2"
+            initial={{ x: transition == false ? 2000 : 0 }}
+            transition={{ type: "tween", duration: 0.38 }}
+            animate={{ x: transition == false ? 0 : 2000 }}
+            >
               <div id="weather"></div>
               <div id="weather-2"></div>
-            </div>
+            </motion.div>
           </div>
         )}
       </div>
--- src/Components/Content/weather.js
+++ src/Components/Content/weather.js
@@ -1,7 +1,14 @@
+import { motion } from "framer-motion";
+import { useState } from "react";
 export default function Weather({toggle}) {
+  const[transition,setTransition]=useState(true);
   return (
-    <div id="weather-container" onClick={()=>toggle()}>
+    <motion.div id="weather-container" onClick={()=>{toggle();setTransition(!transition)}}
+    initial={{scale:0.1,opacity:0}}
+    transition={{type:"tween",delay:0.38,duration:0.38}}
+    animate={{scale:transition==true?1:0.1,opacity:transition==true?1:0}}
+    >
       ok
-    </div>
+    </motion.div>
   );
 }
