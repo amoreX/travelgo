f7f17482ed90c3194a29cf47c08a548dff99916d
Md Nihal Rahman <rahmannihal72@gmail.com>
2024-08-15 19:40:18 +0530

    animation-done

--- src/Components/Content/Row-1/fooddetails.js
+++ src/Components/Content/Row-1/fooddetails.js
@@ -1,21 +1,38 @@
-
-import{useState,useEffect} from "react";
+import { useState, useEffect } from "react";
 import { FoodTry } from "../../Utils/foodtry";
 import { Details } from "../../Utils/fooddesc";
+import { motion } from "framer-motion";
 import { GettingPictures } from "../../Utils/gettingpictures";
 
 export default function Food({ city }) {
-    const[foods,setFoods]=useState(null);
-    const[foodDetails,setFoodDetails]=useState(null);
-    const [error, setError] = useState(null); // Add state to store error
+  const [foods, setFoods] = useState(null);
+  const [foodDetails, setFoodDetails] = useState(null);
+  const [error, setError] = useState(null); // Add state to store error
+
+  const loading = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24px"
+      height="24px"
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <path
+        d="M20.0001 12C20.0001 13.3811 19.6425 14.7386 18.9623 15.9405C18.282 17.1424 17.3022 18.1477 16.1182 18.8587C14.9341 19.5696 13.5862 19.9619 12.2056 19.9974C10.825 20.0328 9.45873 19.7103 8.23975 19.0612"
+        stroke="#204629"
+        strokeWidth="3.55556"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
 
   useEffect(() => {
     const gettingFoods = async () => {
       try {
-        setTimeout(async()=>{
+        setTimeout(async () => {
           let q = await FoodTry(city);
           setFoods(q.split(","));
-        },10000);
+        }, 10000);
       } catch (err) {
         setError(err); // Store the error if it occurs
         console.error("Error fetching food options:", err); // Log the error for debugging
@@ -47,38 +64,55 @@ export default function Food({ city }) {
     }
   }, [foods]);
 
-
-    useEffect(()=>{
-      console.log(foodDetails);
-    },[foodDetails]);
-    return (
-      <div id="foodtotry">
-        <div id="content-foods">
-        {foodDetails !=null?
+  useEffect(() => {
+    console.log(foodDetails);
+  }, [foodDetails]);
+  return (
+    <div id="foodtotry">
+      <div id="content-foods">
+        {foodDetails != null ? (
           foodDetails.map((placeObj, ind) => {
-            const { name, description,picUrl } = placeObj; 
+            const { name, description, picUrl } = placeObj;
             return (
               <div id="foo" key={ind}>
-                <img src={picUrl} alt="loading..."/>
-                <div id="cook-food">
-                <div id="name" style={{fontSize:"20px"}}>{name}</div>
-                <div id="desc" style={{color:"grey",fontSize:"16px"}} >{description.substring(0,60)} {(description.length > 70 ? "..." : null)}</div>
-
-                </div>
+                <motion.img
+                  src={picUrl}
+                  alt="loading..."
+                  initial={{ scale: 0.1, opacity: 0 }}
+                  transition={{
+                    type: "tween",
+                    delay: 0.2+ind * 0.1,
+                    duration: 0.28,
+                  }}
+                  animate={{ scale: 1, opacity: 1 }}
+                />
+                <motion.div id="cook-food" 
+                initial={{y:20,opacity:0}}
+                transition={{type:"tween",delay:0.4+ind*0.1,duration:0.28}}
+                animate={{y:0,opacity:1}}
+                >
+                  <div id="name" style={{ fontSize: "20px" }}>
+                    {name}
+                  </div>
+                  <div id="desc" style={{ color: "grey", fontSize: "16px" }}>
+                    {description.substring(0, 60)}
+                    {description.length > 70 ? "..." : null}
+                  </div>
+                </motion.div>
               </div>
-              
-
-              
             );
           })
-        :
-        <div>loading...</div>
-        }
-        </div>
-
-        <div id="title" style={{ color: "rgba(32, 70, 41, 0.45)", fontSize: "18px" }}>food to try</div>
-
+        ) : (
+          <div id="loading">{loading}</div>
+        )}
       </div>
-    );
-  }
-  
+
+      <div
+        id="title"
+        style={{ color: "rgba(32, 70, 41, 0.45)", fontSize: "18px" }}
+      >
+        food to try
+      </div>
+    </div>
+  );
+}
--- src/Components/Content/Row-1/thingstocarry.js
+++ src/Components/Content/Row-1/thingstocarry.js
@@ -1,6 +1,24 @@
 import { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 import { Etiq } from "../../Utils/getetiq";
 export default function Things({ city }) {
+  const loading = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24px"
+      height="24px"
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <path
+        d="M20.0001 12C20.0001 13.3811 19.6425 14.7386 18.9623 15.9405C18.282 17.1424 17.3022 18.1477 16.1182 18.8587C14.9341 19.5696 13.5862 19.9619 12.2056 19.9974C10.825 20.0328 9.45873 19.7103 8.23975 19.0612"
+        stroke="#204629"
+        strokeWidth="3.55556"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
+
   const [etiq, setEtiq] = useState(null);
 
   useEffect(() => {
@@ -25,8 +43,16 @@ export default function Things({ city }) {
                 <div id="each-thing">
                   {ind <= 6 ? (
                     <>
-                      <span style={{marginLeft:"20px",marginTop:"2px"}}>{each}</span>
-                      <div id="bar"></div>
+                      <motion.span style={{marginLeft:"20px",marginTop:"2px"}}
+                      initial={{x:-20,opacity:0}}
+                      transition={{type:"tween",delay:0.4+ind*0.1,duration:0.38}}
+                      animate={{x:0,opacity:1}}
+                      >{each}</motion.span>
+                      <motion.div id="bar" 
+                      initial={{width:"5%",opacity:0}}
+                      transition={{type:"tween",delay:0.4+ind*0.1,duration:0.38}}
+                      animate={{width:"100%",opacity:1}}
+                      ></motion.div>
                     </>
                   ) : null}
                 </div>
@@ -35,7 +61,7 @@ export default function Things({ city }) {
           </div>
         )}
 
-        {!etiq && <div>loading</div>}
+        {!etiq && <div id="loading">{loading}</div>}
       </div>
       <div id="title" style={{ color: "rgba(32, 70, 41, 0.45)", fontSize: "18px" }}>things to carry</div>
     </div>
