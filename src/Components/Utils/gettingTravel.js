c2857f53a5c0c8b7ff3282b17c6088e229ad9bbd
nihal <rahmannihal72@gmail.com>
2025-03-01 21:03:24 +0530

    works now

--- src/Components/Utils/gettingTravel.js
+++ src/Components/Utils/gettingTravel.js
@@ -1,17 +1,18 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 const cache = {};
 export async function Travel(city) {
-  // console.log(city);
-  // const genAI = new GoogleGenerativeAI("AIzaSyCpSvYvxAYQEGFtfBBMEI4R-9M6V-H_uwM");
-  const genAI = new GoogleGenerativeAI("AIzaSyBerHhY82HxUFAC5xSd5_kiYolPlKA8eQI");
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+	// console.log(city);
+	// const genAI = new GoogleGenerativeAI("AIzaSyCpSvYvxAYQEGFtfBBMEI4R-9M6V-H_uwM");
+	const genAI = new GoogleGenerativeAI("AIzaSyAxWhVJfD7fw1RLuP86qNMbflH2N7gcchY");
+	//new one -> AIzaSyBYm_TyKpW2rrhyOZSekvc1BlUP9_SKJYA
+	const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-  if (cache[city]) {
-    return cache[city];
-  }
-  let prompt = `Tell me about ${city} in 250 characters exact, dont give anything extra in response`;
+	if (cache[city]) {
+		return cache[city];
+	}
+	let prompt = `Tell me about ${city} in 250 characters exact, dont give anything extra in response`;
 
-  const result = await model.generateContent(prompt);
-  cache[city] = result.response.text();
-  return(result.response.text());
+	const result = await model.generateContent(prompt);
+	cache[city] = result.response.text();
+	return result.response.text();
 }
