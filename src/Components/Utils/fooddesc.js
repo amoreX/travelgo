7991ef5ce2798150f548e22620b94819b2f9e010
Nihal Rahman <rahmannihal72@gmail.com>
2025-08-15 16:21:30 +0530

    fixed weather

--- src/Components/Utils/fooddesc.js
+++ src/Components/Utils/fooddesc.js
@@ -2,16 +2,18 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 const cache = {};
 
 export async function Details(place) {
-	// const genAI = new GoogleGenerativeAI("AIzaSyDwh90FSOOwpDCDKomcG2NgGWBVFdzqF4Q");
-	const genAI = new GoogleGenerativeAI("AIzaSyDsbmoRIMVz6e5iCs41dbgPblrj4IlTnyU");
-	const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+  // const genAI = new GoogleGenerativeAI("AIzaSyDwh90FSOOwpDCDKomcG2NgGWBVFdzqF4Q");
+  const genAI = new GoogleGenerativeAI(
+    "AIzaSyDsbmoRIMVz6e5iCs41dbgPblrj4IlTnyU",
+  );
+  const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
-	if (cache[place]) {
-		return cache[place];
-	}
+  if (cache[place]) {
+    return cache[place];
+  }
 
-	let prompt = `give short description about ${place} within 250 characters exact`;
-	const result = await model.generateContent(prompt);
-	cache[place] = result.response.text();
-	return result.response.text();
+  let prompt = `give short description about ${place} within 250 characters exact`;
+  const result = await model.generateContent(prompt);
+  cache[place] = result.response.text();
+  return result.response.text();
 }
